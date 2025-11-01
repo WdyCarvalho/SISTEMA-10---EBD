4c728058f1d3d2defdc0abe7582bc0d64259d4b9
WdyCarvalho <wdycarvalho@hotmail.com>
2025-11-01 01:16:50 -0300

    ERRO DO STATICS, CORRIGIDO

--- config/settings.py
+++ config/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Diz ao Django para procurar arquivos estáticos na pasta 'static' 
 # que acabamos de criar na raiz do projeto.
