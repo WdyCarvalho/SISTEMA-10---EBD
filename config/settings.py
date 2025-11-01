0462d212fec814cf42a11129f013bb97a57c9cce
WdyCarvalho <wdycarvalho@hotmail.com>
2025-11-01 01:22:08 -0300

    STATIC_ROOT E MEDIA_ROOT inseridos

--- config/settings.py
+++ config/settings.py
@@ -124,14 +124,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Diz ao Django para procurar arquivos estáticos na pasta 'static' 
 # que acabamos de criar na raiz do projeto.
-STATICFILES_DIRS = [
+"""STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
+"""
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
